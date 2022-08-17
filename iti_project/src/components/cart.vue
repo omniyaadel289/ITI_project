d27dc6df8462e774f71f3dd39a254ebc90fa24ce
MaZeN8687 <mazeen.zx@gmail.com>
2022-08-17 17:21:19 +0200

    Cart file

---
+++ iti_project/src/components/cart.vue
@@ -0,0 +1,27 @@
+<template>
+<Header/>
+    <div class="card" style="width: 60rem;">
+        <div class="row no-gutters">
+            <div class="col-sm-2">
+                <img class="card-img" src="https://images-americanas.b2w.io/produtos/01/00/item/132165/8/132165801G1.jpg" alt="Suresh Dasari Card">
+            </div>
+            <div class="col-sm-5">
+                <div class="card-body">
+                    <h4 class="card-title">Product name</h4>
+                    <p class="card-text">Produt details</p>
+                    <h6 class="card-text">Produt price$</h6>
+                    <button class="btn btn-danger">Remove</button> 
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Header from './components/header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ iti_project/src/components/smartphones.vue
@@ -0,0 +1,21 @@
+<template>
+<Header/>
+<div class="card my-5" style="width: 18rem;">
+  <img src="" class="card-img-top" >
+  <div class="card-body">
+    <h5 class="card-title text-center "><a class="text-dark" href="#">Product Name</a></h5>
+    <p class="card-text text-center">Product details</p>
+    <h6 class="card-text">Price $</h6>
+    <a href="#" class="btn btn-success" style="width:15rem;">Add to Cart <i class="fa-solid fa-cart-shopping"></i></a>
+  </div>
+</div>
+</template>
+<script>
+import Header from './components/header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
