89c96eeda090e0d731745db70ee3a5a3bfefeae1
AhmedHery <111382796+AhmedHery@users.noreply.github.com>
2022-08-18 18:10:39 +0200

    Update notebookscomponent.vue

--- iti_project/src/components/notebookscomponent.vue
+++ iti_project/src/components/notebookscomponent.vue
@@ -1,11 +1,12 @@
 <template>
-                <div class="d-flex flex-row flex-wrap justify-content-around align-items-baseline">
+                <div class="d-flex flex-row flex-wrap justify-content-between ">
                     <div  class="card my-3" style="width:20rem">
                         <img src="" class="card-img-top product-img" alt="" id="productimg">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">product.name </h5>
-                            <p class="card-text text-center"> product.details </p>
-                            <h6 class="card-title ">product.price $</h6>
+                        <div class="card-body" v-for="notebook in notebook" :key="notebook.price">
+                            <p class="card-text text-center">{{notebook.image}} </p>
+                            <h5 class="card-title text-center">{{notebook.name}} </h5>
+                            <p class="card-text text-center">{{notebook.details}} </p>
+                            <h6 class="card-title ">{{notebook.price}} $</h6>
                             <div class="d-flex justify-content-center align-items-baseline">
                                 <button class="btn btn-success px-5" @click="addProductToCart">Add to cart <i class="fa-solid fa-cart-shopping"></i></button>
                             </div>
@@ -13,4 +14,29 @@
                     </div>
                 </div>
 
-</template>
+</template> 
+<script>
+//  import students from '../students'
+
+import notebookscomponent from './notebookscomponent.vue'
+
+
+export default {
+    async created(){
+      const res= await fetch("http://localhost:3000/notebook");
+      this.notebook=await res.json();
+    },
+    data(){
+        return{
+        notebook : [],
+        }
+    },components:{
+    notebookscomponent,
+},
+    methods: {
+     
+    }
+}
+
+
+</script>
